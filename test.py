c11931d0b7c00e049becd81e50a1d8e8421a5017
Howard <howardtgould@gmail.com>
2021-10-17 00:37:05 -0400

    added interface and destination creation

--- test.py
+++ test.py
@@ -1,3 +1,19 @@
+import time
 import RNS
-import FldigiInterface
+from FldigiInterface import FldigiInterface
+
+def rx_packet(message, packet):
+    print(message)
+
+rns = RNS.Reticulum()
+print('Reticulum started')
+
+interface = FldigiInterface(RNS.Transport, 'xml-rpc', '127.0.0.1', 7362, headless=True)
+RNS.Transport.interfaces.append(interface)
+
+identity = RNS.Identity()
+dest = RNS.Destination(identity, RNS.Destination.IN, RNS.Destination.SINGLE, 'portal', 'fldigi')
+dest.set_packet_callback(rx_packet)
+dest.announce()
+print('announce sent')
 
