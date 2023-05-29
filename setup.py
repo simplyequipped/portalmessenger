21ee2e8a5176f7a092edce4c42bfee0ba5fc94af
OtisByron <howard@simplyequipped.com>
2023-05-28 21:02:33 -0400

    add dependencies

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/simplyequipped/portal",
     packages=setuptools.find_packages(),
+    install_requires=['flask', 'flask-socketio', 'pyjs8call>=0.2.0'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
