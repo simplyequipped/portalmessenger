cd968235d4a0422b61fbd908ee8252af43177202
Simply Equipped <92337033+simplyequipped@users.noreply.github.com>
2023-06-12 17:12:43 -0400

    handle 'error' and 'encrypted' msg attributes

--- static/chat.js
+++ static/chat.js
@@ -1,106 +1,116 @@
-        
-        function newChatMessage(msg) {
-            newMsg = $(".original-hidden").clone();
-            newMsg.attr('id', msg.id)
-            newMsg.addClass('chat-msg-' + msg.type);
-            newMsg.find('.chat-time').attr('data-timestamp', msg.time);
-            newMsg.find('.chat-time').html(timeString(msg.time));
-            newMsg.find('.chat-msg').html(msg.text);
+function newChatMessage(msg) {
+	newMsg = $(".original-hidden").clone();
+	newMsg.attr('id', msg.id)
+	newMsg.addClass('chat-msg-' + msg.type);
+	newMsg.find('.chat-time').attr('data-timestamp', msg.time);
+	newMsg.find('.chat-time').html(timeString(msg.time));
+	newMsg.find('.chat-msg').html(msg.text);
+	
+	if ( msg.encrypted != null ) {
+		if ( msg.type == 'rx' ) {
+			newMsg.find('.chat-time').append('<span class="encrypted-icon">&nbsp;</span>');
+		}
+		else if ( msg.type == 'tx' ) {
+			newMsg.find('.chat-time').prepend('<span class="encrypted-icon">&nbsp;</span>');
+		}
+	}
 
-            if ( msg.status == 'sent' ) {
-                newMsg.find('.chat-status').remove();
-            }
-            else if ( msg.status != null) {
-			    upperCaseStatus = msg.status.charAt(0).toUpperCase() + msg.status.slice(1).toLowerCase();
-                newMsg.find('.chat-status').html(upperCaseStatus);
-            }
-
-            newMsg.removeClass("original-hidden");
-            newMsg.appendTo(".chat-messages");
-        }
-
-        function setTxStatus(id, tx_status) {
-			upperCaseStatus = tx_status.charAt(0).toUpperCase() + tx_status.slice(1).toLowerCase();
-
-			if ( $('#' + id).find('.chat-status').length == 0 ) {
-				$('.original-hidden').find('.chat-status').clone().appendTo('#' + id);
-			}
-
-            $('#'+ id).find('.chat-status').html(upperCaseStatus);
-
-			if ( tx_status == 'sent' ) {
-				setInterval(function () {
-					removeTxStatus(id);
-				}, 5000);
-			}
-        }
-
-        function removeTxStatus(id) {
-            $('#'+ id).find('.chat-status').remove();
-        }
-
-		function scrollChat() {
-			scrollDiv = $(".chat-messages");
-			scrollDiv.scrollTop(scrollDiv.prop("scrollHeight"));
+	if ( msg.status == 'sent' ) {
+		newMsg.find('.chat-status').remove();
+	}
+	else if ( msg.status != null) {
+		upperCaseStatus = msg.status.charAt(0).toUpperCase() + msg.status.slice(1).toLowerCase();
+		
+		if ( msg.error != null ) {
+			upperCaseStatus += ', ' + msg.error.toLowerCase();
 		}
 		
-		function getPresence() {
-			if ( $('.presence-indicator').hasClass("presence-active") ) {
-                return "active";
-            }
-			else if ( $(".presence-indicator").hasClass("presence-inactive") ) {
-                return "inactive";
-            }
-			else if ( $(".presence-indicator").hasClass("presence-unknown") ) {
-                return "unknown";
-            }
-			else {
-                return "none";
-            }
-		}
+		newMsg.find('.chat-status').html(upperCaseStatus);
+	}
 
-		function setPresence(presence) {
-			currentPresence = getPresence();
+	newMsg.removeClass("original-hidden");
+	newMsg.appendTo(".chat-messages");
+}
 
-			if ( currentPresence != "none" ) {
-				$('.presence-indicator').removeClass("presence-" + currentPresence);
-			}
+function setTxStatus(id, tx_status) {
+	upperCaseStatus = tx_status.charAt(0).toUpperCase() + tx_status.slice(1).toLowerCase();
 
-			$('.presence-indicator').addClass("presence-" + presence);
-		}
+	if ( $('#' + id).find('.chat-status').length == 0 ) {
+		$('.original-hidden').find('.chat-status').clone().appendTo('#' + id);
+	}
 
-        function sendMsg(msg_text) {
-            socket.emit('msg', {msg: msg_text});
-        }
+	$('#'+ id).find('.chat-status').html(upperCaseStatus);
 
-		function setLastHeard(last_heard) {
-			now = new Date();
-			then = new Date(last_heard * 1000);
+	if ( tx_status == 'sent' ) {
+		setInterval(function () {
+			removeTxStatus(id);
+		}, 5000);
+	}
+}
 
-			last_heard_minutes = Math.floor( ((now - then) / 1000) / 60 );
+function removeTxStatus(id) {
+	$('#'+ id).find('.chat-status').remove();
+}
 
-			lastHeardElement = $('.last-heard');
-			lastHeardElement.html(lastHeardText(last_heard_minutes));
-			lastHeardElement.attr('data-last-heard', Math.floor(last_heard));
-			updatePresence(last_heard_minutes);
-		}
+function scrollChat() {
+	scrollDiv = $(".chat-messages");
+	scrollDiv.scrollTop(scrollDiv.prop("scrollHeight"));
+}
 
-		function updatePresence(last_heard_minutes) {
-			setPresence(presenceText(last_heard_minutes));
-		}
+function getPresence() {
+	if ( $('.presence-indicator').hasClass("presence-active") ) {
+		return "active";
+	}
+	else if ( $(".presence-indicator").hasClass("presence-inactive") ) {
+		return "inactive";
+	}
+	else if ( $(".presence-indicator").hasClass("presence-unknown") ) {
+		return "unknown";
+	}
+	else {
+		return "none";
+	}
+}
 
-		function sortChat() {
-			var messages = $('.chat-msg-rx, .chat-msg-tx')
+function setPresence(presence) {
+	currentPresence = getPresence();
 
-			messages.sort(function(msgA, msgB) {
-				msgATime = parseInt( $(msgA).find('.chat-time').attr('data-timestamp') );
-				msgBTime = parseInt( $(msgB).find('.chat-time').attr('data-timestamp') );
-				return msgATime - msgBTime;
-			});
+	if ( currentPresence != "none" ) {
+		$('.presence-indicator').removeClass("presence-" + currentPresence);
+	}
 
-			$('.chat-msg-rx, .chat-msg-tx').detach();
-			messages.appendTo('.chat-messages');
-		}
+	$('.presence-indicator').addClass("presence-" + presence);
+}
 
+function sendMsg(msg_text) {
+	socket.emit('msg', {msg: msg_text});
+}
 
+function setLastHeard(last_heard) {
+	now = new Date();
+	then = new Date(last_heard * 1000);
 
+	last_heard_minutes = Math.floor( ((now - then) / 1000) / 60 );
+
+	lastHeardElement = $('.last-heard');
+	lastHeardElement.html(lastHeardText(last_heard_minutes));
+	lastHeardElement.attr('data-last-heard', Math.floor(last_heard));
+	updatePresence(last_heard_minutes);
+}
+
+function updatePresence(last_heard_minutes) {
+	setPresence(presenceText(last_heard_minutes));
+}
+
+function sortChat() {
+	var messages = $('.chat-msg-rx, .chat-msg-tx')
+
+	messages.sort(function(msgA, msgB) {
+		msgATime = parseInt( $(msgA).find('.chat-time').attr('data-timestamp') );
+		msgBTime = parseInt( $(msgB).find('.chat-time').attr('data-timestamp') );
+		return msgATime - msgBTime;
+	});
+
+	$('.chat-msg-rx, .chat-msg-tx').detach();
+	messages.appendTo('.chat-messages');
+}
