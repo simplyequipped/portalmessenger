80502b29414ffe2a3710046648d7bb4e6d3413e5
Simply Equipped <92337033+simplyequipped@users.noreply.github.com>
2023-06-14 11:13:16 -0400

    add abs path to url

--- static/stations.js
+++ static/stations.js
@@ -176,7 +176,7 @@ function stationClick() {
 	var username = $(this).find('.chat-name').html();
 	var presence = getPresence(username);
 	//$.post('/stations', {user: username}, function() {
-	window.location = 'portalmessenger/chat.html?' + $('.tab.selected').attr('id') + '&' + username + '&' + presence;
+	window.location = 'simplyequipped.github.io/portalmessenger/chat.html?' + $('.tab.selected').attr('id') + '&' + username + '&' + presence;
 	//});
 }
 
