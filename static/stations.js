773bd5b4cc5cf33411e7323b421d3b9f7cdfc05b
Simply Equipped <92337033+simplyequipped@users.noreply.github.com>
2023-06-14 10:35:57 -0400

    fix typo

--- static/stations.js
+++ static/stations.js
@@ -1,207 +1,203 @@
+// construct new station element based on given station data
+function newStation(station) {
+	stationElement = $('.station.original-hidden').clone();
+	stationElement.attr('name', station.username)
+	stationElement.find('.chat-name').html(station.username);
+	stationElement.click(stationClick)
+	stationElement.removeClass('original-hidden');
+	stationElement.appendTo('.content');
+	setLastHeard(station.username, station.time);
+}
 
-		// construct new station element based on given station data
-		function newStation(station) {
-			stationElement = $('.station.original-hidden').clone();
-			stationElement.attr('name', station.username)
-			stationElement.find('.chat-name').html(station.username);
-			stationElement.click(stationClick)
-			stationElement.removeClass('original-hidden');
-			stationElement.appendTo('.content');
-			setLastHeard(station.username, station.time);
+// construct new conversation station element based on given username
+function newConversation(station) {
+	stationElement = $('.station.original-hidden').clone();
+	stationElement.attr('name', username)
+	stationElement.find('.chat-name').html(username);
+	stationElement.click(stationClick)
+	stationElement.removeClass('original-hidden');
+	stationElement.appendTo('.content');
+}
+
+// create or update spot stations sent from the server
+function handleSpot(station) {
+	if ( findStation(station.username).length == 0 ) {
+		newStation(station);
+		stationElement = findStation(station.username);
+		stationElement.addClass('spot');
+
+		if ( selectedTab() != 'activity' ) {
+			stationElement.hide();
 		}
+	}
+	else {
+		setLastHeard(station.username, station.time);
+		findStation(station.username).addClass('spot');
+	}
+}
 
-		// construct new conversation station element based on given username
-		function newConversation(station) {
-			stationElement = $('.station.original-hidden').clone();
-			stationElement.attr('name', username)
-			stationElement.find('.chat-name').html(username);
-			stationElement.click(stationClick)
-			stationElement.removeClass('original-hidden');
-			stationElement.appendTo('.content');
+// create or update conversation stations sent from the server
+function handleConversation(station) {
+	if ( findStation(station.username).length == 0 ) {
+		newStation(station);
+		stationElement = findStation(station.username);
+		stationElement.addClass('conversation');
+
+		if ( selectedTab() != 'conversations' ) {
+			stationElement.hide();
 		}
+	}
+	else {
+		setLastHeard(station.username, station.time);
+		findStation(station.username).addClass('conversation');
+	}
 
-		// create or update spot stations sent from the server
-		function handleSpot(station) {
-			if ( findStation(station.username).length == 0 ) {
-				newStation(station);
-				stationElement = findStation(station.username);
-			    stationElement.addClass('spot');
+	if ( station.unread ) {
+		markUnread(station.username);
+	}
+	else {
+		markRead(station.username);
+	}
+}
 
-				if ( selectedTab() != 'activity' ) {
-				    stationElement.hide();
-				}
-			}
-			else {
-				setLastHeard(station.username, station.time);
-				findStation(station.username).addClass('spot');
-			}
+// find and return the station DOM element based on given username 
+function findStation(username) {
+	return $(".station[name='" + username + "']");
+}
+
+// get station presence status based on given username
+function getPresence(username) {
+	station = findStation(username)
+	if ( station.find('.presence-indicator').hasClass("presence-active") ) {
+		return "active";
+	}
+	else if ( station.find(".presence-indicator").hasClass("presence-inactive") ) {
+		return "inactive";
+	}
+	else if ( station.find(".presence-indicator").hasClass("presence-unknown") ) {
+		return "unknown";
+	}
+	else {
+		return "none";
+	}
+}
+
+// set station to given presence based on given username
+function setPresence(username, presence) {
+	station = findStation(username);
+	presenceElement = station.find('.presence-indicator');
+	currentPresence = getPresence(username);
+
+	if ( getPresence(username) !== "none" ) {
+		presenceElement.removeClass("presence-" + currentPresence);
+	}
+	presenceElement.addClass("presence-" + presence);
+}
+
+// determine and set presence based on minutes since last heard
+function updatePresence(username, last_heard_minutes) {
+	setPresence(username, presenceText(last_heard_minutes));
+}
+
+// update station last heard time and presence based on given
+// username and minutes since last heard
+function setLastHeard(username, lastHeard) {
+	if ( lastHeard == null ) {
+		lastHeard = 0;
+	}
+
+	now = new Date();
+	then = new Date(lastHeard * 1000);
+	lastHeardMinutes = Math.floor( ((now - then) / 1000) / 60 );
+
+	stationLastHeard = findStation(username).find('.last-heard');
+	stationLastHeard.attr('data-last-heard', Math.floor(lastHeard));
+	stationLastHeard.html(lastHeardText(lastHeardMinutes));
+	updatePresence(username, lastHeardMinutes);
+}
+
+// mark station as read based on username
+function markRead(username) {
+	stationChatName = findStation(username).find('.chat-name');
+
+	if ( stationChatName.length > 0 && stationChatName.hasClass('unread') ) {
+		stationChatName.removeClass('unread');
+		// increment stored unread count
+		unreadCount = parseInt( $('#unread-count').attr('data-unread-count') );
+		unreadCount--;
+		$('#unread-count').attr('data-unread-count', unreadCount);
+
+		// update and show/hide unread count
+		if ( unreadCount > 0 ) {
+			$('#unread-count').html('(' + unreadCount + ')');
+			$('#unread-count').show();
 		}
-
-		// create or update conversation stations sent from the server
-		function handleConversation(station) {
-			if ( findStation(station.username).length == 0 ) {
-				newStation(station);
-				stationElement = findStation(station.username);
-			    stationElement.addClass('conversation');
-
-				if ( selectedTab() != 'conversations' ) {
-				    stationElement.hide();
-				}
-			}
-			else {
-				setLastHeard(station.username, station.time);
-				findStation(station.username).addClass('conversation');
-			}
-
-			if ( station.unread ) {
-				markUnread(station.username);
-			}
-			else {
-				markRead(station.username);
-			}
+		else {
+			$('#unread-count').hide();
 		}
+	}
+}
 
-		// find and return the station DOM element based on given username 
-		function findStation(username) {
-			return $(".station[name='" + username + "']");
+// mark stations as unread based on username
+function markUnread(username) {
+	stationChatName = findStation(username).find('.chat-name');
+
+	if ( stationChatName.length > 0 && !stationChatName.hasClass('unread') ) {
+		stationChatName.addClass('unread');
+		// increment stored unread count
+		unreadCount = parseInt( $('#unread-count').attr('data-unread-count') );
+		unreadCount++;
+		$('#unread-count').attr('data-unread-count', unreadCount);
+
+		// update and show/hide unread count
+		if ( unreadCount > 0 ) {
+			$('#unread-count').html('(' + unreadCount + ')');
+			$('#unread-count').show();
 		}
-
-		// get station presence status based on given username
-		function getPresence(username) {
-			station = findStation(username)
-			if ( station.find('.presence-indicator').hasClass("presence-active") ) {
-				return "active";
-			}
-			else if ( station.find(".presence-indicator").hasClass("presence-inactive") ) {
-				return "inactive";
-			}
-			else if ( station.find(".presence-indicator").hasClass("presence-unknown") ) {
-				return "unknown";
-			}
-			else {
-				return "none";
-			}
+		else {
+			$('#unread-count').hide();
 		}
+	}
+}
 
-		// set station to given presence based on given username
-		function setPresence(username, presence) {
-			station = findStation(username);
-			presenceElement = station.find('.presence-indicator');
-			currentPresence = getPresence(username);
+function selectedTab() {
+	tab = $('.tab.selected').attr('id');
 
-			if ( getPresence(username) !== "none" ) {
-				presenceElement.removeClass("presence-" + currentPresence);
-			}
-			presenceElement.addClass("presence-" + presence);
-		}
+	if ( tab == 'tab-conversations' ) {
+		return 'conversations';
+	}
+	else if ( tab == 'tab-activity' ) {
+		return 'activity';
+	}
+}
 
-		// determine and set presence based on minutes since last heard
-		function updatePresence(username, last_heard_minutes) {
-			setPresence(username, presenceText(last_heard_minutes));
-		}
+// on click event handler for stations div
+function stationClick() {
+	var username = $(this).attr('name');
+	var presence = getPresence(username);
+	//$.post('/stations', {user: username}, function() {
+	window.location = '/chat?' + $('.tab.selected').attr('id') + '&' + username + '&' + presence;
+	//});
+}
 
-		// update station last heard time and presence based on given
-		// username and minutes since last heard
-		function setLastHeard(username, lastHeard) {
-            if ( lastHeard == null ) {
-                lastHeard = 0;
-            }
+function conversationHover() {
+	if ( selectedTab() == 'conversations' ) {
+		console.log('hover');
+		$(this).find('.last-heard').toggle();
+		$(this).find('.icon-conversation-settings').toggle();
+	}
+}
 
-			now = new Date();
-		    then = new Date(lastHeard * 1000);
-		    lastHeardMinutes = Math.floor( ((now - then) / 1000) / 60 );
+// sort stations in ascending order by last heard time
+function sortStations() {
+	var stations = $('.station').not('.original-hidden');
 
-			stationLastHeard = findStation(username).find('.last-heard');
-		    stationLastHeard.attr('data-last-heard', Math.floor(lastHeard));
-		    stationLastHeard.html(lastHeardText(lastHeardMinutes));
-		    updatePresence(username, lastHeardMinutes);
-		}
+	stations.sort(function(stationA, stationB) {
+		stationALastHeard = parseInt( $(stationA).find('.last-heard').attr('data-last-heard') );
+		stationBLastHeard = parseInt( $(stationB).find('.last-heard').attr('data-last-heard') );
+		return stationBLastHeard - stationALastHeard;
+	});
 
-		// mark station as read based on username
-		function markRead(username) {
-			stationChatName = findStation(username).find('.chat-name');
-
-			if ( stationChatName.length > 0 && stationChatName.hasClass('unread') ) {
-				stationChatName.removeClass('unread');
-				// increment stored unread count
-				unreadCount = parseInt( $('#unread-count').attr('data-unread-count') );
-				unreadCount--;
-				$('#unread-count').attr('data-unread-count', unreadCount);
-
-				// update and show/hide unread count
-				if ( unreadCount > 0 ) {
-					$('#unread-count').html('(' + unreadCount + ')');
-					$('#unread-count').show();
-				}
-				else {
-					$('#unread-count').hide();
-				}
-			}
-		}
-
-		// mark stations as unread based on username
-		function markUnread(username) {
-			stationChatName = findStation(username).find('.chat-name');
-
-			if ( stationChatName.length > 0 && !stationChatName.hasClass('unread') ) {
-				stationChatName.addClass('unread');
-				// increment stored unread count
-				unreadCount = parseInt( $('#unread-count').attr('data-unread-count') );
-				unreadCount++;
-				$('#unread-count').attr('data-unread-count', unreadCount);
-
-				// update and show/hide unread count
-				if ( unreadCount > 0 ) {
-					$('#unread-count').html('(' + unreadCount + ')');
-					$('#unread-count').show();
-				}
-				else {
-					$('#unread-count').hide();
-				}
-			}
-		}
-
-		function selectedTab() {
-			tab = $('.tab.selected').attr('id');
-
-			if ( tab == 'tab-conversations' ) {
-				return 'conversations';
-			}
-			else if ( tab == 'tab-activity' ) {
-				return 'activity';
-			}
-		}
-
-		// on click event handler for stations div
-		function stationClick() {
-		    var username = $(this).attr('name');
-			var presence = getPresence(username);
-            //$.post('/stations', {user: username}, function() {
-            window.location = '/chat?' + $('.tab.selected').attr('id') + '&' + username + '&' + presence;
-			//});
-		}
-
-		function conversationHover() {
-			if ( selectedTab() == 'conversations' ) {
-				console.log('hover');
-				$(this).find('.last-heard').toggle();
-				$(this).find('.icon-conversation-settings').toggle();
-			}
-		}
-
-		// sort stations in ascending order by last heard time
-		function sortStations() {
-			var stations = $('.station').not('.original-hidden');
-
-			stations.sort(function(stationA, stationB) {
-				stationALastHeard = parseInt( $(stationA).find('.last-heard').attr('data-last-heard') );
-				stationBLastHeard = parseInt( $(stationB).find('.last-heard').attr('data-last-heard') );
-				return stationBLastHeard - stationALastHeard;
-			});
-
-			$('.station').not('.original-hidden').detach();
-			stations.appendTo('.content');
-		}
-
-
-
+	$('.station').not('.original-hidden').detach();
+	stations.appendTo('.content');
+}
