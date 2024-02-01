63c3358ccc3a9296eeb143d7616d4c6432885f80
Simply Equipped <92337033+simplyequipped@users.noreply.github.com>
2024-02-01 16:36:43 -0500

    WIP, overhaul

--- portalmessenger/portal.py
+++ portalmessenger/portal.py
@@ -5,14 +5,11 @@ import subprocess
 import time
 
 from flask import Blueprint, render_template, request, session, redirect, current_app
-from flask_socketio import SocketIO
 
 import portalmessenger
 from portalmessenger import db
-from portalmessenger.settings import settings
 
-socketio = SocketIO(current_app)
-
+socketio = current_app.config['SOCKETIO']
 
 
 ### flask routes and template handling
@@ -24,14 +21,14 @@ def stations_route():
     current_app.config[]
     
     if request.method == 'POST':
-         current_app.config['ACTIVE_CHAT_USERNAME'] = request.form.get('user')
+         current_app.config['ACTIVE_CHAT_USER'] = request.form.get('user')
         return ''
     else:
         if db.get_setting_value('callsign') == '':
             return redirect('/settings')
 
-        current_app.config['ACTIVE_CHAT_USERNAME'] = None
-        return render_template('stations.html', settings = db.get_settings())
+        current_app.config['ACTIVE_CHAT_USER'] = None
+        return render_template( 'stations.html', settings = db.get_settings() )
 
 @app.route('/network')
 @app.route('/network.html')
@@ -42,14 +39,15 @@ def network_route():
 @app.route('/chat.html')
 def chat_route():
     mark_user_msgs_read(current_app.config['ACTIVE_CHAT_USERNAME'])
-    return render_template('chat.html', user = current_app.config['ACTIVE_CHAT_USERNAME'], settings = db.get_settings())
+    return render_template('chat.html', user = current_app.config['ACTIVE_CHAT_USER'], settings = db.get_settings())
 
 @app.route('/settings', methods=['GET', 'POST'])
 @app.route('/settings.html', methods=['GET', 'POST'])
 def settings_route():
     if request.method == 'POST':
-        db_settings = db.get_settings()
+        settings = db.get_settings()
         restart = False
+        error_msg = []
         #TODO WIP here
 
         # process posted settings
@@ -57,111 +55,104 @@ def settings_route():
             if setting == 'callsign' or setting == 'grid':
                 value = value.upper()
 
-            if setting in db_settings.keys() and value != db_settings[setting]['value']:
-                valid, error = settings.set(setting, value)
-                db_settings[setting]['error'] = error
+            if setting in settings.keys() and value != settings[setting]['value']:
+                # see settings.py for settings dict and validation criteria
+                valid = portalmessenger.settings.settings[setting]['validate'](value)
 
                 if valid:
-                    db_settings[setting]['value'] = value
+                    db.set_setting(setting, value)
 
-                    if modem.name.lower() == 'js8call':
+                    if current_app.config['MODEM'].name.lower() == 'js8call':
                         # update settings in js8call
                         if setting == 'callsign':
-                            modem.js8call.settings.set_station_callsign(value)
+                            current_app.config['MODEM'].js8call.settings.set_station_callsign(value)
                             restart = True
                         elif setting == 'speed':
-                            modem.js8call.settings.set_speed(value)
+                            current_app.config['MODEM'].js8call.settings.set_speed(value)
                             restart = True
                         elif setting == 'grid':
-                            modem.js8call.settings.set_station_grid(value)
+                            current_app.config['MODEM'].js8call.settings.set_station_grid(value)
                         elif setting == 'freq':
-                            modem.js8call.settings.set_freq(value)
+                            current_app.config['MODEM'].js8call.settings.set_freq(value)
                         elif setting == 'heartbeat':
                             if value == 'enable':
-                                modem.js8call.heartbeat.enable()
+                                current_app.config['MODEM'].js8call.heartbeat.enable()
                             else:
-                                modem.js8call.heartbeat.disable()
-                        # TODO
+                                current_app.config['MODEM'].js8call.heartbeat.disable()
+                        #TODO
                         #elif setting == 'encryption':
                         #    if value == 'enable':
-                        #        modem.enable_encryption()
+                        #        current_app.config['MODEM'].enable_encryption()
                         #    else:
-                        #        modem.disable_encryption()
+                        #        current_app.config['MODEM'].disable_encryption()
 
-                    elif modem.name.lower() == 'demo':
+                    elif current_app.config['MODEM'].name.lower() == 'demo':
                         if setting == 'callsign':
-                            modem.callsign = value
+                            current_app.config['MODEM'].callsign = value
+    
+                else:
+                    settings[setting]['error'] = 'Invalid {}'.format(setting)
 
-        if modem.name.lower() == 'js8call' and restart:
-            #TODO is this the right place to restart? should client side drive this?
-            modem.restart()
+        if restart:
+            current_app.config['MODEM'].restart()
 
     #TODO get server IP address at app init
-    return render_template('settings.html', settings = db_settings, ip = None)
+    return render_template('settings.html', settings = settings, ip = 'IP unavailable')
 
 
 
 ### socket.io functions
 
-@socketio.on('msg')
-def tx_msg(data):
-    global modem
-
-    msg = process_msg( modem.send(data['user'], data['text']) )
-    socketio.emit('msg', [msg])
-    
+# log to terminal
 @socketio.on('log')
 def log(data):
-    print('\tlog: ' + str(data['msg']))
+    print( '\tlog: {}'.format(data['msg']) )
 
-@socketio.on('heard-user')
-def heard_user(data):
-    last_heard = user_last_heard_timestamp(data['user'])
-    socketio.emit('heard-user', last_heard)
+# outgoing message
+@socketio.on('msg')
+def tx_msg(data):
+    msg = current_app.config['MODEM'].send(data['user'], data['text'])
+    msg = process_message(msg)
+    # include processed message in active chat
+    socketio.emit('msg', [msg])
 
-# initilize spot data
+# initilize station spot data
 @socketio.on('spot')
-def update_spots():
-    global modem
-    global settings
-    spots = []
-
+def init_spots():
     # spots since aging setting
-    age = 60 * int(settings.get('aging')) # convert minutes to seconds
-    spots = modem.get_spots(age = age)
+    age = 60 * db.get_setting_value('aging') # convert minutes to seconds
+    spots = current_app.config['MODEM'].get_spots(age = age)
 
     if len(spots) > 0:
         spots = [{'username': spot.origin, 'time': spot.timestamp} for spot in spots]
         socketio.emit('spot', spots)
 
-    #TODO test, no longer needed?
-    #if len(spots) > 0:
-    #    heard(spots)
-
+# get stored chat conversations: dict(username, last heard timestamp, unread count)
 @socketio.on('conversation')
-def update_conversations():
-    conversations = get_stored_conversations()
+def init_conversations():
+    conversations = db.get_user_conversations( db.get_setting_value('callsign') )
+
+    for i in range(len(conversations)):
+        spots = current_app.config['MODEM'].get_spots(origin = conversations[i]['username'], count = 1)
+
+        # use latest station spot timestamp if more recent
+        if len(spots) > 0 and spots[0].timestamp > conversations[i]['time']:
+             conversations[i]['time'] = spots[0].timestamp
+    
     socketio.emit('conversation', conversations)
 
 @socketio.on('init-chat')
 def init_chat():
-    global settings
-    active_chat_username = settings.get('active_chat_username')
-    logged_in_username = settings.get('callsign')
-
-    msgs = user_chat_history(active_chat_username, logged_in_username)
+    msgs = db.get_user_chat_history( current_app.config['ACTIVE_CHAT_USERNAME'], db.get_setting_value('callsign') )
     socketio.emit('msg', msgs)
     
 @socketio.on('network')
 def network_data():
-    global settings
-    global modem
-
     # activity since aging setting
-    activity = modem.js8call.get_call_activity( age = int(settings.get('aging')) )
+    activity = current_app.config['MODEM'].js8call.get_call_activity( age = db.get_setting_value('aging') )
     network = []
 
-    for station in activity: 
+    for station in activity:
         # set spot attributes to non-blank space if not set, formatted value otherwise
         if station['grid'] == '':
             grid = '&nbsp;'
@@ -206,144 +197,39 @@ def network_data():
 
     socketio.emit('network', network)
 
-@socketio.on('get-settings')
-def update_setting():
-    global settings
-    socketio.emit('get-settings', settings.settings())
-
 @socketio.on('power-on')
 def power_on():
-    global modem
-
     # wait for current transactions to finish
     time.sleep(0.1)
-    if not modem.online():
+    if not current_app.config['MODEM'].online():
         # returns once application is started
-        modem.start()
+        current_app.config['MODEM'].start()
 
     socketio.emit('power-on')
 
 @socketio.on('power-off')
 def power_off():
-    global modem
-
     # wait for current transactions to finish
     time.sleep(0.1)
-    modem.stop()
+    current_app.config['MODEM'].stop()
 
     socketio.emit('power-off')
 
 @socketio.on('power-restart')
 def power_restart():
-    global modem
-
     # wait for current transactions to finish
     time.sleep(0.1)
     socketio.emit('power-off')
     # returns once application restarts
-    modem.restart()
+    current_app.config['MODEM'].restart()
     socketio.emit('power-on')
 
 
 
 ### helper functions
 
-# directed message callback
-# because this function is called via callback it does not automatically have the flask request context
-def incoming_msg(msg):
-    global settings
-    active_chat_username = settings.get('active_chat_username')
-    msg = process_msg(msg)
-
-    # pass messages for active chat user to client side
-    if msg['origin'] == active_chat_username:
-        socketio.emit('msg', [msg])
-
-    unread = bool(user_unread_count(msg['origin']))
-
-    # pass conversation to client side
-    conversation = {
-        'username': msg['origin'], 
-        'time': msg['time'],
-        'unread': unread
-    }
-
-    socketio.emit('conversation', [conversation])
-
-def mark_user_msgs_read(username):
-    query('UPDATE messages SET unread = 0 WHERE origin = :user', {'user': username})
-
-def user_unread_count(username):
-    unread = query('SELECT COUNT(*) FROM messages WHERE origin = :user AND unread = 1', {'user': username}).fetchone()
-    return int(unread[0])
-
-def user_last_heard_timestamp(username):
-    # get latest spot timestamp
-    global modem
-
-    # get user spots
-    spots = modem.get_spots(origin = username)
-
-    if spots is not None and len(spots) > 0:
-        last_spot_timestamp = spots[-1].timestamp
-    else:
-        last_spot_timestamp = 0
-
-    # get latest msg timestamp
-    last_msg_timestamp = query('SELECT MAX(time) FROM messages WHERE origin = :user', {'user': username}).fetchone()
-
-    if last_msg_timestamp not in [None, (None,)] and len(last_msg_timestamp) > 0:
-        last_msg_timestamp = last_msg_timestamp[0]
-    else:
-        last_msg_timestamp = 0
-
-    # return the greater of the two timestamps
-    return max(last_spot_timestamp, last_msg_timestamp)
-
-def user_chat_history(user_a, user_b):
-    users = {'user_a': user_a, 'user_b': user_b}
-    # select both sides of the conversation for the given users
-    columns = ['id', 'origin', 'destination', 'type', 'time', 'text', 'unread', 'status', 'error', 'encrypted']
-    msgs = query('SELECT * FROM messages WHERE (origin = :user_a AND destination = :user_b) OR (origin = :user_b AND destination = :user_a)', users).fetchall()
-    msgs = [dict(zip(columns, msg)) for msg in msgs]
-    return msgs
-    
-def get_stored_conversations():
-    global settings
-    conversations = []
-    logged_in_username = settings.get('callsign')
-
-    users = query('SELECT DISTINCT origin, destination FROM messages WHERE origin = :user OR destination = :user', {'user': logged_in_username}).fetchall()
-
-    # if there are no messages to process, end processing
-    if users is None or len(users) == 0:
-        return []
-        
-    # parse result row tuples into single list of unique values
-    users = list(set([user for x in users for user in (x[0], x[1])]))
-    users.remove(logged_in_username)
-
-    for user in users:
-        unread = user_unread_count(user)
-        last_heard = user_last_heard_timestamp(user)
-
-        conversation = {
-            'username': user,
-            'time': last_heard,
-            'unread': bool(unread)
-        }
-
-        conversations.append(conversation)
-
-    return conversations
-
-def new_spots(spots):
-    spots = [{'username': spot.origin, 'time': spot.timestamp} for spot in spots]
-    socketio.emit('spot', spots)
-
-def process_msg(msg):
-    global settings
-
+# msg = pyjs8call.Message object
+def process_message(msg):
     msg = {
         'id': msg.id,
         'origin': msg.origin,
@@ -357,53 +243,51 @@ def process_msg(msg):
         'status': msg.status
         }
 
-    if msg['type'] == 'rx' and msg['origin'] != settings.get('active_chat_username'):
+    if msg['type'] == 'rx' and msg['origin'] != current_app.config['ACTIVE_CHAT_USER']:
         msg['unread'] = True
 
     if msg['type'] == 'tx':
-        msg['origin'] = settings.get('callsign')
+        msg['origin'] = db.get_setting_value('callsign')
 
         if msg['status'] != 'failed':
             msg['status'] = 'queued'
     
-    query('INSERT INTO messages VALUES (:id, :origin, :destination, :type, :time, :text, :unread, :status, :error, :encrypted)', msg)
+    db.store_message(msg)
 
     return msg
 
+# incoming directed message callback
+#TODO confirm: this function does not have the flask request context because it is called via callback
+def incoming_message(msg):
+    msg = process_message(msg)
+
+    if msg['origin'] == current_app.config['ACTIVE_CHAT_USER']:
+    # pass messages for active chat to client side
+        socketio.emit('msg', [msg])
+
+    unread = bool( db.get_user_unread_message_count(msg['origin']) )
+
+    conversation = {
+        'username': msg['origin'], 
+        'time': msg['time'],
+        'unread': unread
+    }
+
+    # pass conversation to client side
+    socketio.emit('conversation', [conversation])
+
+# new spots callback
+def new_spots(spots):
+    spots = [{'username': spot.origin, 'time': spot.timestamp} for spot in spots]
+    socketio.emit('spot', spots)
+    
+# outgoing message status change callback
 def outgoing_status(msg):
     # works inconsistently without this delay, root cause unknown
     time.sleep(0.001)
-
-    query('UPDATE messages SET status = :status WHERE id = :id', {'id': msg.id, 'status': msg.status})
+    db.update_outgoing_message_status(msg)
     socketio.emit('update-tx-status', {'id': msg.id, 'status': msg.status})
 
-
-
-### database functions
-
-def init_db():
-        tables = query('SELECT name FROM sqlite_master').fetchall()
-        tables = [tables[i][0] for i in range(len(tables))]
-
-        if 'messages' not in tables:
-            query('CREATE TABLE messages(id, origin, destination, type, time, text, unread, status, error, encrypted)')
-        
-def query(query, parameters=None):
-    global settings
-
-    with sqlite3.connect(settings.get('db_file'), detect_types=sqlite3.PARSE_DECLTYPES) as con:
-        sqlite3.register_adapter(bool, int)
-        sqlite3.register_converter('BOOLEAN', lambda v: v != '0')
-        db = con.cursor()
-
-        if parameters is None:
-            result = db.execute(query)
-        else:
-            result = db.execute(query, parameters)
-
-        con.commit()
-
-    return result
 
 
 
@@ -417,65 +301,14 @@ def query(query, parameters=None):
 #args = parser.parse_args()
 
 ### initialize settings ###
-db_file = 'portal.db'
-settings = portalmessenger.Settings(db_file)
-#settings.set('headless', args.headless)
-settings.set('headless', True)
 
+#TODO
 # get local IP address
-try:
-    devnull = open(os.devnull, 'w')
-    local_ip = subprocess.check_output(['hostname', '-I'], stderr = devnull).decode('utf-8').split(' ')[0]
-except (FileNotFoundError, subprocess.CalledProcessError):
-    local_ip = 'IP unavailable'
-
-settings.set('ip', local_ip)
-
-### initialize database ###
-init_db()
-
-### initialize modem ###
-#if args.demo:
-#    modem = 'DemoModem'
-#else:
-modem = settings.get('modem')
-
-#TODO force demo modem
-#modem = 'DemoModem'
-
-if modem == 'JS8Call':
-    modem = portalmessenger.JS8CallModem(settings.get('callsign'), headless=settings.get('headless'))
-    # initialize config settings before start
-    modem.js8call.settings.set_speed( settings.get('speed') )
-    # set callback functions
-    modem.incoming = incoming_msg
-    modem.spots = new_spots
-    modem.outgoing = outgoing_status
-    
-    modem.start()
-
-    # initialize settings with running application
-    modem.js8call.settings.set_station_grid( settings.get('grid') )
-    modem.js8call.settings.set_freq( int( settings.get('freq') ) )
-
-    if settings.get('heartbeat') == 'enable':
-        modem.js8call.heartbeat.enable()
-
-elif modem == 'FSKModem':
-    # modem = portalmessenger.FSKModem()
-
-    # TODO
-    # start QDX CAT control module
-    # start fskmodem
-    
-    pass
-
-elif modem == 'DemoModem':
-    modem = portalmessenger.DemoModem( settings.get('callsign') )
-    # set callback functions
-    modem.incoming = incoming_msg
-    modem.spots = new_spots
-    modem.outgoing = outgoing_status
+#try:
+#    devnull = open(os.devnull, 'w')
+#    local_ip = subprocess.check_output(['hostname', '-I'], stderr = devnull).decode('utf-8').split(' ')[0]
+#except (FileNotFoundError, subprocess.CalledProcessError):
+#    local_ip = 'IP unavailable'
 
 
     
@@ -492,7 +325,4 @@ if __name__ == 'main':
     ## exit once application is stopped from web app
     #while not modem.js8call.restarting and modem.js8call.online:
     #    time.sleep(1)
-    
-    input('Press Enter to quit Portal Messenger...')
-    modem.js8call.stop()
 
