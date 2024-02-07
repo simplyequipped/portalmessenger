55bbf7f9f9a06a17522d2805037160d7dfa83cdf
Simply Equipped <92337033+simplyequipped@users.noreply.github.com>
2024-02-07 09:51:29 -0500

    implement waitress wsgi server

--- portalmessenger/__main__.py
+++ portalmessenger/__main__.py
@@ -2,22 +2,30 @@ import os
 import sys
 import argparse
 
+import waitress
+
 from . import create_app
 
 if __name__ == '__main__':
     program = 'python -m portalmessenger'
-    epilog = 'Note: other arguments specified when using --shortcut are included in the shortcut command'
+    epilog = 'Note: --host and --port are combined and passed to the Waitress listen argument (ex. \'*:8080\')'
+    epilog += 'Note: other arguments specified when using --shortcut are included in the shortcut command'
+    
     parser = argparse.ArgumentParser(prog=program, description='Messaging web app using pyjs8call', epilog=epilog)
-    parser.add_argument('-a', '--host', help='Accept requests from this address, defaults to 0.0.0.0 (all hosts)', default='0.0.0.0')
-    parser.add_argument('-p', '--port', help='Web server port, defaults to 5000', default=5000, type=int)
-    parser.add_argument('-d', '--debug', help='Enable debug output (development use only)', action='store_true')
+    parser.add_argument('-a', '--host', help='Accept requests from this host address, defaults to * (wildcard, all hosts)', default='*')
+    parser.add_argument('-p', '--port', help='Web server port, defaults to 8080', default=8080, type=int)
+    #parser.add_argument('-d', '--debug', help='Enable debug output (development use only)', action='store_true')
+    parser.add_argument('-j', '--headless', help='Run JS8Call app headless (Linux only)', action='store_true')
     parser.add_argument('-s', '--shortcut', help='Make desktop shortcut, then exit', action='store_true')
     args = parser.parse_args()
+    
+    listen = '{}:{}'.format(args.host, args.port)
 
     if args.shortcut:
         import pyshortcuts
         _os = pyshortcuts.uname # win, linux, or darwin
-        
+
+        #TODO create icon files, pyshortcuts falls back to python icon
         if _os in ['win', 'linux']:
             icon_file = '/static/icons/portalmessenger.ico'
         elif _os == 'darwin':
@@ -38,5 +46,7 @@ if __name__ == '__main__':
         print('\nDesktop shortcut created, exiting\n')
         exit()
     
-    app, websockets = create_app()
-    websockets.run(app, host=args.host, port=args.port, debug=args.debug, allow_unsafe_werkzeug=True)
+    app, websockets = create_app(headless=args.headless)
+    #websockets.run(app, host=args.host, port=args.port, debug=args.debug, allow_unsafe_werkzeug=True)
+    waitress.serve(app, listen=listen)
+    
