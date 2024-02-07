a468d9efaf77808efb9d29d0a929636bae3bc40c
Simply Equipped <92337033+simplyequipped@users.noreply.github.com>
2024-02-07 14:53:29 -0500

    remove waitress

--- portalmessenger/__main__.py
+++ portalmessenger/__main__.py
@@ -2,24 +2,20 @@ import os
 import sys
 import argparse
 
-import waitress
-
 from . import create_app
 
 if __name__ == '__main__':
     program = 'python -m portalmessenger'
-    epilog = 'Note: --host and --port are combined and passed to the Waitress listen argument (ex. \'*:8080\')'
-    epilog += 'Note: other arguments specified when using --shortcut are included in the shortcut command'
+    #epilog = 'Note: --host and --port are combined and passed to the Waitress listen argument (ex. \'*:8080\')'
+    epilog = 'Note: other arguments specified when using --shortcut are included in the shortcut command'
     
     parser = argparse.ArgumentParser(prog=program, description='Messaging web app using pyjs8call', epilog=epilog)
-    parser.add_argument('-a', '--host', help='Accept requests from this host address, defaults to * (wildcard, all hosts)', default='*')
-    parser.add_argument('-p', '--port', help='Web server port, defaults to 8080', default=8080, type=int)
-    #parser.add_argument('-d', '--debug', help='Enable debug output (development use only)', action='store_true')
+    parser.add_argument('-a', '--host', help='Accept requests from this host address, defaults to 0.0.0.0 (all hosts)', default='0.0.0.0')
+    parser.add_argument('-p', '--port', help='Web server port, defaults to 5000', default=5000, type=int)
+    parser.add_argument('-d', '--debug', help='Enable debug output (development use only)', action='store_true')
     parser.add_argument('-j', '--headless', help='Run JS8Call app headless (Linux only)', action='store_true')
     parser.add_argument('-s', '--shortcut', help='Make desktop shortcut, then exit', action='store_true')
     args = parser.parse_args()
-    
-    listen = '{}:{}'.format(args.host, args.port)
 
     if args.shortcut:
         import pyshortcuts
@@ -47,6 +43,5 @@ if __name__ == '__main__':
         exit()
     
     app, websockets = create_app(headless=args.headless)
-    #websockets.run(app, host=args.host, port=args.port, debug=args.debug, allow_unsafe_werkzeug=True)
-    waitress.serve(app, listen=listen)
+    websockets.run(app, host=args.host, port=args.port, debug=args.debug, allow_unsafe_werkzeug=True)
     
