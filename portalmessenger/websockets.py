25ff6dc5dd6e00e7b11b217f518ddb5c3ad3a835
OtisByron <howard@simplyequipped.com>
2024-02-07 23:04:21 -0500

    handle tx only conversations where no last heard timestamp exists

--- portalmessenger/websockets.py
+++ portalmessenger/websockets.py
@@ -45,7 +45,9 @@ def init_conversations():
         spots = current_app.config['MODEM'].get_spots(origin = conversations[i]['username'], count = 1)
 
         # use latest station spot timestamp if more recent
-        if conversations[i]['time'] is None or (len(spots) > 0 and spots[0].timestamp > conversations[i]['time']):
+        if conversations[i]['time'] is not None and len(spots) > 0 and spots[0].timestamp > conversations[i]['time']):
+             conversations[i]['time'] = spots[0].timestamp
+        elif conversations[i]['time'] is None and len(spots) > 0:
              conversations[i]['time'] = spots[0].timestamp
     
     socketio.emit('conversation', conversations)
