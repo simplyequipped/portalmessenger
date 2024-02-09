f97feb376fb085c0cc192cafcfee0c25d9481325
Simply Equipped <92337033+simplyequipped@users.noreply.github.com>
2024-02-09 17:56:47 -0500

    pass debug arg to pyjs8call, utilize modem update functions

--- portalmessenger/__init__.py
+++ portalmessenger/__init__.py
@@ -7,7 +7,7 @@ from flask import Flask
 
 
 # app factory
-def create_app(test_config=None, headless=True, pyjs8call_config_path=None):
+def create_app(test_config=None, headless=True, debugging=False, pyjs8call_config_path=None):
     # create and configure the app
     app = Flask(__name__)
     app.config['SECRET_KEY'] = secrets.token_hex()
@@ -32,30 +32,29 @@ def create_app(test_config=None, headless=True, pyjs8call_config_path=None):
         # initalize pyjs8call modem
         from .modem.js8callmodem import JS8CallModem
         #TODO set callsign after init for consistency
-        app.config['MODEM'] = JS8CallModem(db.get_setting_value('callsign'), headless=headless)
+        app.config['MODEM'] = JS8CallModem()
 
         # load pyjs8call config file
         if pyjs8call_config_path is not None:
-            app.config['MODEM'].js8call.load_config(pyjs8call_config_path)
-            
+            app.config['MODEM'].load_config(pyjs8call_config_path)
+
+        
+        app.config['MODEM'].()
+        app.config['MODEM'].(
         # initialize pyjs8call config before start (see pyjs8call.settings docs)
-        app.config['MODEM'].js8call.settings.set_speed( db.get_setting_value('speed') )
+        callsign = db.get_setting_value('callsign')
+        if callsign not in [None, '']:
+            app.config['MODEM'].update_callsign(callsign)
+        app.config['MODEM'].update_speed(db.get_setting_value('speed'))
         # start pyjs8call modem
         print('Starting JS8Call modem via pyjs8call...')
-        app.config['MODEM'].start()
+        app.config['MODEM'].start(headless=headless, debugging=debugging)
         print('JS8Call started')
-        
         # initialize running modem application settings
-        app.config['MODEM'].js8call.settings.set_station_grid( db.get_setting_value('grid') )
-        app.config['MODEM'].js8call.settings.set_freq( db.get_setting_value('freq') )
-    
-        if db.get_setting_value('heartbeat') == 'enable':
-            app.config['MODEM'].js8call.heartbeat.enable()
-            
-        if db.get_setting_value('inbox') == 'enable':
-            app.config['MODEM'].js8call.inbox.enable()
-        elif 'query' in db.get_setting_value('inbox'):
-            app.config['MODEM'].js8call.inbox.enable(query=True)
+        app.config['MODEM'].update_freq(db.get_setting_value('freq'))
+        app.config['MODEM'].update_grid(db.get_setting_value('grid'))
+        app.config['MODEM'].update_heartbeat(db.get_setting_value('heartbeat'))
+        app.config['MODEM'].update_inbox(db.get_setting_value('inbox'))
 
     def app_context_aware(func):
         def _wrapped_function(*args, **kwargs):
