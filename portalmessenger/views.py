ee30c3f86ad3dd7f001abea38187ded8d4ab0590
Simply Equipped <92337033+simplyequipped@users.noreply.github.com>
2024-02-16 11:19:45 -0500

    add propagation map view

--- portalmessenger/views.py
+++ portalmessenger/views.py
@@ -17,7 +17,7 @@ def stations_route():
 
         if '>' in username:
             # remove spaces around relay characters
-            username = '>'.join( [user.strip() for user in username.split('>')] )
+            username = '>'.join([user.strip() for user in username.split('>')])
         
         current_app.config['ACTIVE_CHAT_USER'] = username
         return ''
@@ -26,24 +26,23 @@ def stations_route():
             return redirect('/settings')
 
         current_app.config['ACTIVE_CHAT_USER'] = None
-        return render_template('stations.html', settings = db.get_settings() )
+        return render_template('stations.html', settings = db.get_settings())
 
 @bp.route('/quit')
 @bp.route('/quit.html')
 def quit_route():
-    return render_template('quit.html', settings = db.get_settings() )
-
+    return render_template('quit.html', settings = db.get_settings())
 
 @bp.route('/network')
 @bp.route('/network.html')
 def network_route():
-    return render_template('network.html', settings = db.get_settings() )
+    return render_template('network.html', settings = db.get_settings())
 
 @bp.route('/chat')
 @bp.route('/chat.html')
 def chat_route():
     db.set_user_messages_read(current_app.config['ACTIVE_CHAT_USER'])
-    return render_template('chat.html', user = current_app.config['ACTIVE_CHAT_USER'], settings = db.get_settings() )
+    return render_template('chat.html', user = current_app.config['ACTIVE_CHAT_USER'], settings = db.get_settings())
 
 @bp.route('/settings', methods=['GET', 'POST'])
 @bp.route('/settings.html', methods=['GET', 'POST'])
@@ -52,12 +51,16 @@ def settings_route():
         # validate and update posted settings
         updated_settings = settings.update_settings(request.form)
 
-        # retsart modem if required
-        if any( [updated_settings[setting]['restart'] for setting in updated_settings] ):
+        # restart modem if required
+        if any([updated_settings[setting]['restart'] for setting in updated_settings]):
             current_app.config['MODEM'].restart()
 
         return render_template('settings.html', settings = updated_settings, ip = current_app.config['LOCAL_IP'])
         
     return render_template('settings.html', settings = db.get_settings(), ip = current_app.config['LOCAL_IP'])
 
-
+@bp.route('/propagation')
+@bp.route('/propagation.html')
+def network_route():
+    return render_template('propagation.html', settings = db.get_settings())
+    
