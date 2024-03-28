f92e89758025e30fb06b761570a07460cf9a3be5
Simply Equipped <92337033+simplyequipped@users.noreply.github.com>
2024-03-28 12:05:40 -0400

    delay 2 sec before opening browser
    added redirect arg and http server to redirect from port 80 to flask port, but commented out for now

--- portalmessenger/__main__.py
+++ portalmessenger/__main__.py
@@ -1,10 +1,24 @@
 import os
 import sys
 import time
+#import atexit
 import argparse
+import threading
 import webbrowser
 
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
 from . import create_app
+
+
+#class RedirectHTTPRequestHandler(BaseHTTPRequestHandler):
+#    def do_GET(self):
+#        host_header = self.headers.get('Host')
+#        
+#        if host_header.endswith('.local'):
+#            self.send_response(302)
+#            self.send_header('Location', 'http://{}:{}{}'.format(host_header, redirect_port, self.path)
+#            self.end_headers()
 
 
 if __name__ == '__main__':
@@ -19,6 +33,7 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--debug', help='Enable pyjs8call debug output', action='store_true')
     parser.add_argument('-b', '--browser', help='Open a browser window to 127.0.0.1 after starting server', action='store_true')
     parser.add_argument('-s', '--shortcut', help='Create a desktop shortcut to launch the application, then exit', action='store_true')
+    #parser.add_argument('-s', '--redirect', help='Redirect hostname.local requests from port 80 to the web server port', action='store_true')
     args = parser.parse_args()
     sys_args = ' '.join(sys.argv[1:])
 
@@ -41,10 +56,26 @@ if __name__ == '__main__':
 
         print('\nDesktop shortcut created, exiting\n')
         exit()
+
+#    if args.redirect:
+#        httpd = HTTPServer(('', 80), RedirectHTTPRequestHandler)
+#        atexit.register(httpd.shutdown)
+#        print('Starting HTTP redirect server on port 80...')
+#
+#        thread = threading.Thread(target=httpd.serve_forever)
+#        thread.daemon = True
+#        thread.start()
     
     app, websockets = create_app(headless=args.headless, debugging=args.debug, pyjs8call_settings_path=args.settings)
 
     if args.browser:
-        webbrowser.open('http://127.0.0.1:{}'.format(args.port))
+        def delay_opening_browser():
+            time.sleep(2)
+            webbrowser.open('http://127.0.0.1:{}'.format(args.port))
+
+        thread = threading.Thread(target=delay_opening_browser)
+        thread.daemon = True
+        thread.start()
 
     websockets.run(app, host=args.host, port=args.port, debug=args.debug, allow_unsafe_werkzeug=True)
+    
