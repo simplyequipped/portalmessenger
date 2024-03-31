60f846883d06baa381a0687125f6b446237444e6
OtisByron <howard@simplyequipped.com>
2024-03-31 17:55:01 -0400

    fix issue where shortcut arg included in shortcut command

--- portalmessenger/__main__.py
+++ portalmessenger/__main__.py
@@ -23,19 +23,18 @@ from . import create_app
 
 if __name__ == '__main__':
     program = 'python -m portalmessenger'
-    epilog = 'Note: other arguments specified when using --shortcut are included in the shortcut command'
+    epilog = 'Note: arguments specified when using --shortcut are included in the shortcut command'
     
     parser = argparse.ArgumentParser(prog=program, description='Messaging web app using pyjs8call', epilog=epilog)
     parser.add_argument('-a', '--host', help='Accept requests from this host address, defaults to 0.0.0.0 (all hosts)', default='0.0.0.0')
     parser.add_argument('-p', '--port', help='Web server port, defaults to 5000', default=5000, type=int)
     parser.add_argument('-j', '--headless', help='Run JS8Call app headless (Linux only)', action='store_true')
-    parser.add_argument('-c', '--settings', help='Path to pyjs8call settings file')
+    parser.add_argument('-s', '--settings', help='Path to pyjs8call settings file')
     parser.add_argument('-d', '--debug', help='Enable pyjs8call debug output', action='store_true')
     parser.add_argument('-b', '--browser', help='Open a browser window to 127.0.0.1 after starting server', action='store_true')
-    parser.add_argument('-s', '--shortcut', help='Create a desktop shortcut to launch the application, then exit', action='store_true')
+    parser.add_argument('-c', '--shortcut', help='Create a desktop shortcut to launch the application, then exit', action='store_true')
     #parser.add_argument('-s', '--redirect', help='Redirect hostname.local requests from port 80 to the web server port', action='store_true')
     args = parser.parse_args()
-    sys_args = ' '.join(sys.argv[1:])
 
     if args.shortcut:
         import pyshortcuts
@@ -49,8 +48,9 @@ if __name__ == '__main__':
             
         icon_path = os.path.join( os.path.dirname( os.path.abspath(__file__) ), icon_file)
         
-        # include specified args in pyshortcuts command, removing --shortcut
-        sys_args = sys_args.replace('--shortcut ', '').replace('-s ', '').replace('  ', ' ').strip()
+        # include specified args in pyshortcuts command, removing shortcut args
+        sys_args = [arg for arg in sys.argv[1:] if arg.strip() not in ('--shortcut', '-c')]
+        sys_args = ' '.join(sys_args)
         command = '{} -m portalmessenger ' + sys_args
         pyshortcuts.make_shortcut(command, name='Portal Messenger', icon=icon_path, terminal=True)
 
