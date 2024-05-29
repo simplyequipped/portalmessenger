8714e89a91bcf4a0f8e2ccee5822247d8fb99289
OtisByron <howard@simplyequipped.com>
2024-05-28 22:08:00 -0400

    fix incoming group msg being marked unread while in group conv

--- portalmessenger/message.py
+++ portalmessenger/message.py
@@ -27,19 +27,16 @@ def process_message(msg):
     }
 
     if msg['type'] == 'rx':
-        if msg['destination'].startswith('@') and msg['destination'] != current_app.config['ACTIVE_CHAT_USER']:
+        if msg['destination'].startswith('@'):
+            # handle group message
+            if msg['destination'] != current_app.config['ACTIVE_CHAT_USER']:
+                msg['unread'] = True
 
-            #TODO test code, quotes to check for leading/trailing spaces
-            print('\'' + msg['destination'] + '\'')
-            print('\'' + current_app.config['ACTIVE_CHAT_USER'] + '\'')
-            
-            msg['unread'] = True
+            # prepend origin to msg text
+            msg['text'] = '{}: {}'.format(msg['origin'], msg['text'])
+
         elif msg['origin'] != current_app.config['ACTIVE_CHAT_USER']:
             msg['unread'] = True
-
-        if msg['destination'].startswith('@'):
-            # msg to group, prepend origin to msg text
-            msg['text'] = '{}: {}'.format(msg['origin'], msg['text'])
 
     if msg['type'] == 'tx':
         msg['origin'] = db.get_setting_value('callsign')
