d52a163552cb1ac92c15b9a2e7e76a9d175612bf
OtisByron <howard@simplyequipped.com>
2024-06-09 15:49:20 -0400

    prevent view cacheing

--- portalmessenger/views.py
+++ portalmessenger/views.py
@@ -1,5 +1,5 @@
 import threading
-from flask import Blueprint, render_template, request, redirect, current_app
+from flask import Blueprint, render_template, request, redirect, current_app, make_response
 
 import portalmessenger
 from portalmessenger import db
@@ -8,7 +8,18 @@ from portalmessenger import settings
 
 bp = Blueprint('portalmessenger', __name__)
 
+# decorator for view functions to set headers to prevent cacheing
+def no_cache(view):
+    def no_cache_view(*args, **kwargs):
+        response = make_response(view(*args, **kwargs))
+        response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0"
+        response.headers["Pragma"] = "no-cache"
+        response.headers["Expires"] = "0"
+        return response
+    return no_cache_view
 
+
+@no_cache
 @bp.route('/')
 @bp.route('/stations', methods=['GET', 'POST'])
 @bp.route('/stations.html', methods=['GET', 'POST'])
@@ -29,22 +40,26 @@ def stations_route():
         current_app.config['ACTIVE_CHAT_USER'] = None
         return render_template('stations.html', settings = db.get_settings())
 
+@no_cache
 @bp.route('/quit')
 @bp.route('/quit.html')
 def quit_route():
     return render_template('quit.html', settings = db.get_settings())
 
+@no_cache
 @bp.route('/network')
 @bp.route('/network.html')
 def network_route():
     return render_template('network.html', settings = db.get_settings())
 
+@no_cache
 @bp.route('/chat')
 @bp.route('/chat.html')
 def chat_route():
     db.set_user_messages_read(current_app.config['ACTIVE_CHAT_USER'])
     return render_template('chat.html', user = current_app.config['ACTIVE_CHAT_USER'], settings = db.get_settings())
 
+@no_cache
 @bp.route('/settings', methods=['GET', 'POST'])
 @bp.route('/settings.html', methods=['GET', 'POST'])
 def settings_route():
@@ -69,10 +84,9 @@ def settings_route():
         
     return render_template('settings.html', settings = db.get_settings(), ip = current_app.config['LOCAL_IP'], status='view')
 
+@no_cache
 @bp.route('/propagation')
 @bp.route('/propagation.html')
 def propagation_route():
     return render_template('propagation.html', settings = db.get_settings())
-
-
     
